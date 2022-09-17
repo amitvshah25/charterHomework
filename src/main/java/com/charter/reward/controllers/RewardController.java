9200561efed155e890ef44497f2fe49abbbf82b6
Amit Shah <amit.v.shah.25@gmail.com>
2022-09-17 13:43:22 -0400

    moved aggregation to service layer

--- src/main/java/com/charter/reward/controllers/RewardController.java
+++ src/main/java/com/charter/reward/controllers/RewardController.java
@@ -22,35 +22,6 @@ public class RewardController {
 
     @PostMapping(value = "/reward", consumes = {"application/json"})
     public Map<String, Integer> calculateReward(@RequestBody List<Transaction> transactionList) {
-        Map<String, List<Transaction>> groupedTransactions = transactionList
-                .stream()
-                .map(parseMonthName)
-                .collect(Collectors.groupingBy(Transaction::getTransactionMonth));
-        Map<String, Integer> monthlyRewardPointsMap = groupedTransactions.entrySet()
-                .stream()
-                .map(e -> Map.entry(e.getKey(), e.getValue()
-                        .stream()
-                        .reduce(0, rewardPointsFromTransaction, Integer::sum)))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue ));
-        Integer overallTotal = monthlyRewardPointsMap.values().stream().reduce(0, Integer::sum);
-        monthlyRewardPointsMap.put("OVERALL_TOTAL", overallTotal);
-        return monthlyRewardPointsMap;
+        return rewardService.getMonthlyRewardPoints(transactionList);
     }
-
-    Function<Transaction, Transaction> parseMonthName = new Function<Transaction, Transaction>() {
-        @Override
-        public Transaction apply(Transaction t) {
-            return new Transaction(
-                    t.getTransactionDate(),
-                    t.getTransactionAmount(),
-                    t.getTransactionDate().getMonth().name(), rewardService.getPoints(t.getTransactionAmount()));
-        }
-    };
-
-    BiFunction<Integer, Transaction, Integer> rewardPointsFromTransaction = new BiFunction<Integer, Transaction, Integer>() {
-        @Override
-        public Integer apply(Integer rewardPoints, Transaction t) {
-            return t.getPoints() + rewardPoints;
-        }
-    };
 }
--- src/main/java/com/charter/reward/services/RewardService.java
+++ src/main/java/com/charter/reward/services/RewardService.java
@@ -1,6 +1,13 @@
 package com.charter.reward.services;
 
+import com.charter.reward.models.Transaction;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class RewardService {
@@ -16,4 +23,36 @@ public class RewardService {
         }
     }
 
+    public Map<String, Integer> getMonthlyRewardPoints(List<Transaction> transactionList) {
+        Map<String, List<Transaction>> groupedTransactions = transactionList
+                .stream()
+                .map(parseMonthName)
+                .collect(Collectors.groupingBy(Transaction::getTransactionMonth));
+        Map<String, Integer> monthlyRewardPointsMap = groupedTransactions.entrySet()
+                .stream()
+                .map(e -> Map.entry(e.getKey(), e.getValue()
+                        .stream()
+                        .reduce(0, rewardPointsFromTransaction, Integer::sum)))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue ));
+        Integer overallTotal = monthlyRewardPointsMap.values().stream().reduce(0, Integer::sum);
+        monthlyRewardPointsMap.put("OVERALL_TOTAL", overallTotal);
+        return monthlyRewardPointsMap;
+    }
+
+    Function<Transaction, Transaction> parseMonthName = new Function<Transaction, Transaction>() {
+        @Override
+        public Transaction apply(Transaction t) {
+            return new Transaction(
+                    t.getTransactionDate(),
+                    t.getTransactionAmount(),
+                    t.getTransactionDate().getMonth().name(), getPoints(t.getTransactionAmount()));
+        }
+    };
+
+    BiFunction<Integer, Transaction, Integer> rewardPointsFromTransaction = new BiFunction<Integer, Transaction, Integer>() {
+        @Override
+        public Integer apply(Integer rewardPoints, Transaction t) {
+            return t.getPoints() + rewardPoints;
+        }
+    };
 }
